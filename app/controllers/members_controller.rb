b5cf86ee23f65eaed4b618b8baedef53752d71bf
Patrick Tuszakowski <mrtall500@gmail.com>
2021-04-15 17:52:25 -0400

    MVP

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -10,9 +10,5 @@ class MembersController < ApplicationController
         render json: @member
     end
 
-    # def compare
-    #     @member_one = Member.find(params[:cid1])
-    #     @member_two = Member.find(params[:cid2])
-    #     render json: @member_one, @member_two
-    # end
+
 end
